dbde70ab993cf5306b241131e282d8fddc1e96ba
Fawwaz Topasi <fawwaztopasy0000@gmail.com>
2023-08-23 19:08:56 +0300

    delete some useless pages

--- lib/themes/themes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:starterapp/constants/style/style_constants.dart';
-
-ThemeData lightTheme = ThemeData(
-    useMaterial3: true,
-    primaryColor: StyleConstants.colorOfApp,
-    scaffoldBackgroundColor: StyleConstants.scaffoldBackgroundColor);
